1448db47c89601eb1b1121eb8c8613d32b533464
Nergiz Özge ERDAĞI <nergiz.erdagi@std.ankaramedipol.edu.tr>
2021-01-21 19:15:42 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            int i = 0;
+            while (i < tempKeys.Length)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+                i++;
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        private static bool value;
+
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Pencil", "Kurşun Kalem");
+            myDictionary.Add("Eraser", "Silgi");
+            myDictionary.Add("Pencil Case", "Kalemlik");
+           
+
+            foreach (var value in myDictionary.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            myDictionary.Add("School Bag", "Okul Çantası");
+            foreach (var value in myDictionary.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            myDictionary.Add("Glass", "Bardak");
+            foreach (var value in myDictionary.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            
+        }
+    }
+}
